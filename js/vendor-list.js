42babe08e74a2a99a952fab2d8bdd716ec0076ed
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-26 09:36:39 -0400

    add other version vendor list

--- js/vendor-list.js
+++ js/vendor-list.js
@@ -4,11 +4,9 @@ $().ready(() => {
 
     vendorList()
         .done((vendors) => {
-            console.log(vendors);
-            allVendors = vendors;
-            display(allVendors);
+            console.log("Vendors:", vendors);
+            display(vendors);
         })
-        .fail()
 
 })
 
@@ -17,16 +15,30 @@ const display = (vendors) => {
     tbody.empty();
     for(let vendor of vendors) {
         let tr = $("<tr></tr>");
-        let tdId = $(`<td>${vendor.id}</td>`);
-        tr.append(tdId);
+        tr.append( $(`<td>${vendor.id}</td>`) );
         tr.append($(`<td>${vendor.code}</td>`));
         tr.append($(`<td>${vendor.name}</td>`));
-        tr.append($(`<td>${vendor.address}</td>`));
-        tr.append($(`<td>${vendor.city}</td>`));
-        tr.append($(`<td>${vendor.state}</td>`));
-        tr.append($(`<td>${vendor.zip}</td>`));
-        tr.append($(`<td>${vendor.phone}</td>`));
-        tr.append($(`<td>${vendor.email}</td>`));
+        let tdAddr = $(`<td>${vendor.address}, ${vendor.city}, ${vendor.state} ${vendor.zip}</td> `);
+        tr.append(tdAddr)
         tbody.append(tr);
     }
 }
+
+// const display = (vendors) => {
+//     let tbody = $("tbody");
+//     tbody.empty();
+//     for(let vendor of vendors) {
+//         let tr = $("<tr></tr>");
+//         let tdId = $(`<td>${vendor.id}</td>`);
+//         tr.append(tdId);
+//         tr.append($(`<td>${vendor.code}</td>`));
+//         tr.append($(`<td>${vendor.name}</td>`));
+//         tr.append($(`<td>${vendor.address}</td>`));
+//         tr.append($(`<td>${vendor.city}</td>`));
+//         tr.append($(`<td>${vendor.state}</td>`));
+//         tr.append($(`<td>${vendor.zip}</td>`));
+//         tr.append($(`<td>${vendor.phone}</td>`));
+//         tr.append($(`<td>${vendor.email}</td>`));
+//         tbody.append(tr);
+//     }
+// }
